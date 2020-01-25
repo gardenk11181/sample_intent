b8f1f4444fe2acc576ca8f80b9e96ed7806178b4
gardenk11181 <55073876+gardenk11181@users.noreply.github.com>
2020-01-25 21:27:24 +0900

    Intent Master

--- app/src/main/java/com/gardenlab/sampleintent/MainActivity.java
+++ app/src/main/java/com/gardenlab/sampleintent/MainActivity.java
@@ -1,14 +1,31 @@
 package com.gardenlab.sampleintent;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     public static final int REQUEST_CODE_MENU=101;
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(requestCode == REQUEST_CODE_MENU) {
+            Toast.makeText(this, "onActivityResult 메서드 호출됨. 요청코드 : "+ requestCode +", 결과코드 : "
+            + resultCode, Toast.LENGTH_SHORT).show();
+            if(resultCode == RESULT_OK) {
+                String name = data.getStringExtra("name");
+                Toast.makeText(this, "응답으로 전달된 name : "+ name,
+                        Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
